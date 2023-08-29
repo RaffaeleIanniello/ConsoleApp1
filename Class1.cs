c1711a201c8b9640ff7f3dcc75e24e633e6a1a52
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-08-29 15:44:36 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public double _età;
+        public double Età
+        {
+            get { return _età; }
+            set { _età = value; }
+
+
+        }
+        public string FullName
+        {
+            get
+            {
+                return Nome + " " + Cognome;
+            }
+        }
+        public Persona(string c, string n)
+        {
+            Nome = n;
+            Cognome = c;
+        }
+        public Persona(string _nome, string _cognome, double _età)
+        {
+            Nome = _nome;
+            Cognome = _cognome;
+            Età = _età;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il nome della persona:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il cognome della persona:");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci l'età della persona:");
+            double età = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
